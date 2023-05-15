9b718de6c316bdebc563efb3a2eb5383f47b792e
Gowthami Kumaresan <demo.pwens@gmail.com>
2023-05-15 14:13:19 +0100

    Added UI

--- read.js
+++ read.js
@@ -1,16 +1,15 @@
 import data from "./MOCK_DATA.json" assert { type: "json" };
 console.log(data);
 let employees = data;
-let html = "<tbody>";
+let html = "";
 for (let employee of employees) {
   html += `<tr>`;
   html += `<td>${employee.employee_number}</td>`;
   html += `<td>${employee.first_name} ${employee.last_name}</td>`;
   html += `<td>${employee.address},${employee.city},${employee.country}</td>`;
-
   html += `<td>${employee.salary}</td>`;
   html += `<td>${employee.role}</td>`;
   html += `</tr>`;
 }
-html += "</tbody>";
+
 document.getElementById("box").innerHTML = html;
