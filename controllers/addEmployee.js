a70019e9022121eb37bdcbb9913b0c9537d9654b
Farhad Panahi <fpanahi01@qub.ac.uk>
2023-05-16 11:36:46 +0100

    refactored the structure of the code added add new employee functionality

---
+++ controllers/addEmployee.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const employees = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+const employee_number = employees.length + 1;
+
+const getAddEmployee = (req, res) => {
+    res.render('addEmployee.ejs');
+}
+
+const addEmployee = (req, res) => {
+    console.log(req.body);
+    const first_name = req.body.firstNameField;
+    const last_name = req.body.lastNameField;
+    const address = req.body.addressField;
+    const city = req.body.cityField;
+    const country = req.body.countryField;
+    const post_code = req.body.postCodeField;
+    const salary = req.body.salaryField;
+    const role = req.body.RoleField;
+
+    const newEmployee = {
+        employee_number,
+        first_name,
+        last_name,
+        address,
+        city,
+        country,
+        post_code,
+        salary,
+        role
+    };
+    employees.push(newEmployee);
+    const jsonEmployees = JSON.stringify(employees);
+    fs.writeFileSync('./data.json', jsonEmployees, 'utf8');
+    fs.closeSync(fs.openSync('./data.json', 'r'));
+    res.redirect('/employees');
+    }
+
+module.exports = {
+    getAddEmployee,
+    addEmployee,
+};
+    
