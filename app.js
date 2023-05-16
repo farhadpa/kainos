75c6ca2bc1f4f6f77b87c9dcc7ae3eff9e54c832
Gowthami Kumaresan <demo.pwens@gmail.com>
2023-05-16 14:01:42 +0100

    added login page

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const getEmployees = require('./controllers/employees');
-const addEmployee = require('./controllers/addEmployee');
-const deleteEmployee = require('./controllers/deleteEmployee');
-const editEmployee = require('./controllers/editEmployee');
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const getEmployees = require("./controllers/employees");
+const addEmployee = require("./controllers/addEmployee");
+const deleteEmployee = require("./controllers/deleteEmployee");
+const editEmployee = require("./controllers/editEmployee");
 
 const app = express();
 const port = 3500;
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const JSON_FILE_PATH = './data.json';
+const JSON_FILE_PATH = "./data.json";
 
-app.get('/', (req, res) => {
-    res.render('home.ejs');
-}
-);
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-app.get('/employees', getEmployees);
-app.get('/addEmployee', addEmployee.getAddEmployee);
-app.post('/addEmployee', addEmployee.addEmployee);
-app.get('/deleteEmployee', deleteEmployee.deleteEmployeeById);
-app.get('/updateEmployee', editEmployee.getUpdateEmployeeById);
-app.post('/updateEmployee', editEmployee.updateEmployeeById);
+app.get("/employees", getEmployees);
+app.get("/addEmployee", addEmployee.getAddEmployee);
+app.post("/addEmployee", addEmployee.addEmployee);
+app.get("/deleteEmployee", deleteEmployee.deleteEmployeeById);
+app.get("/updateEmployee", editEmployee.getUpdateEmployeeById);
+app.post("/updateEmployee", editEmployee.updateEmployeeById);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-}
-);
+  console.log(`Example app listening at http://localhost:${port}`);
+});
